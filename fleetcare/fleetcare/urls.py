b349cf90e1101a2a49c0eda1e26ab114aec31a2a
Alexander2203 <topuser567@gmail.com>
2025-12-10 12:00:00 +0300

    Выполнение миграций

--- fleetcare/fleetcare/urls.py
+++ fleetcare/fleetcare/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core.api import router as api_router
 
 urlpatterns = [
     
     # Админка
     path("admin/", admin.site.urls),
+    
+    # Телега
+    path("api/", include(api_router.urls)),
 ]
